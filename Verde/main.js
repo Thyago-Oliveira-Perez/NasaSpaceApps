045fa8055210594c7f85d2ed88e51ff8b87cbc73
Pedro Henrique <pehdfms@gmail.com>
2021-10-03 09:50:22 -0300

    adiciona API, me chama no pv pra pegar a chave

---
+++ Verde/main.js
@@ -0,0 +1,23 @@
+var chave = config.CHAVE_API;
+
+const getCurrentWeather = async (cityid) => {
+    var request = "https://api.openweathermap.org/data/2.5/weather?id=" + cityid + "&appid=" + config.CHAVE_API + "&units=metric";
+
+    return await fetch(request);
+}
+
+const updateInfo = async (cityid) => {
+    const response = await getCurrentWeather(cityid);
+    const myJson = await response.json();
+
+    var temperatura = parseInt(myJson["main"]["temp"]);
+    var clima = myJson["weather"][0]["main"];
+    var vento = myJson["wind"]["speed"];
+
+    document.getElementById("temperatura").innerHTML = "TEMPERATURA : " + temperatura + "ºC";
+    document.getElementById("extraTemperatura").innerHTML = "Temperatura : " + temperatura + "ºC";
+    document.getElementById("clima").innerHTML = "Clima : " + clima;
+    document.getElementById("vento").innerHTML = "Vento : " + vento + "km/h";
+}
+
+res = updateInfo("3463030");
