43a1ee6b82919c71c4a3e3f89b1430dc1453b63f
Thyago de Oliveira Perez <thyagodoliveiraperez@gmail.com>
2021-10-03 11:39:28 -0300

    extensao

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("botao").addEventListener("click", paginaQueVaiAbrir);
+
+function paginaQueVaiAbrir(){
+
+	var page1 = 1
+	var page2 = 2
+	var page3 = 3
+
+	if(page1 < page3){
+
+		window.open("pageGood.html");
+				
+	}
+
+}
